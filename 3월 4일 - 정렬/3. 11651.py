149df3f1609cf01ddfcc6834e6f4f71e3eae2aa1
Yujin Seol <66419086+rkdfc93@users.noreply.github.com>
2022-03-11 23:58:40 +0900

    [정렬] 3월 10일

--- 3월 4일 - 정렬/3. 11651.py
+++ 3월 4일 - 정렬/3. 11651.py
@@ -1,14 +1,10 @@
+import sys      #sys 모듈 불러오기
+input = sys.stdin.readline  #입력받기 설정
 
+n = int(input())    #점의 개수 입력받기
+arr = [list(map(int, input().split())) for _ in range(n)]   #점의 개수만큼 반복하여 입력받아 리스트에 저장
+# 정렬의 우선순위에 맞춰 람다함수를 작성합니다.
+arr.sort(key=lambda x:(x[1], x[0])) #주어진 기준에 맞춘 람다힘수로 리스트를 정렬한다.
 
-N = int(input())
-dot = [[0 for col in range(N)] for row in range(2)]
-
-for i in range(N):
-    dot[0][i], dot[1][i] = map(int, input().split())
-
-dot[1].sort()
-
-for i in range(N):
-    for j in range(2):
-        print(dot[j][i], end=' ')
-    print(" ")
+for x, y in arr:    #리스트 속의 x, y값들을
+    print(x, y)     #출력한다.
--- 3월 4일 - 정렬/4. 1341.py
+++ 3월 4일 - 정렬/4. 1341.py
@@ -1,35 +1,25 @@
-import re
-from functools import cmp_to_key
+import sys      #sys 모듈 불러오기
+input = sys.stdin.readline      #입력받기 설정
 
-def sum(list):
-    sum = 0
-    for i in list:
-        sum += int(i)
-    return sum
+n = int(input())    #기타 개수 입력받아 저장
+arr = [input().rstrip() for _ in range(n)]  #시리얼번호를 기타 개수만큼 반복하여 입력받는다.
+#rstrip - 인자로 전달된 문자를 string의 오른쪽에서 제거, 전달하지 않으면 공백 제거
+#lstrip(왼쪽), strip(양쪽)
 
-def compare(a, b):
-    if len(a) == len(b):
-        a_num = re.findall("\d", a)
-        b_num = re.findall("\d", b)
-        if sum(a_num)<sum(b_num):
-            return 1;
-        elif sum(a_num)>sum(b_num):
-            return -1;
-        else:
-            return(a<b)
-            #사전
-    elif len(a) < len(b):
-        return 1;
-    else:
-        return -1;
+def sum_digit(text):
+    # 문자열에 포함된 숫자를 전부 더해 반환하는 함수입니다.
+    total = 0   #합계 초기화
+    for c in text:  #문자열 속 문자 하나마다
+        # 문자열의 메소드 .isdigit()으로 숫자인지 판단할 수 있습니다.
+        if c.isdigit(): #숫자인지 아닌지 판단하고, 만약 숫자라면
+            total += int(c) #그 수를 합계에 더한다.
+    return total    #계산한 합계 반환
 
-N = int(input())
-serial = [0 for i in range(N)]
+# 정렬의 우선순위에 맞춰서 람다함수를 작성합니다.
+# 첫번째는 문자열의 길이, 두번째가 숫자의 합, 세번째가 사전순 입니다.
+arr.sort(key=lambda x:(len(x), sum_digit(x), x))
 
-for i in range(N):
-    serial[i] = input()
-
-#print(compare(serial[1], serial[3]))
-
-sorted = sorted(serial, key=cmp_to_key(compare))
-print(sorted)
+for i in arr:   #정렬한 시리얼 번호들을 하나씩
+    print(i)    #출력한다
+# 혹은 print('\n'.join(arr)) 로 한줄에 처리할 수 있습니다.
+# .join 메소드는 문자열을 처리할 때 유용하니 알아두세요!
